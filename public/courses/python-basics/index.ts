95f30f152edcf91f004b0aa6faa697d0c5ac2372
José Scott <je.paes.scott@gmail.com>
2025-06-30 20:57:49 -0300

    feat: remover arquivos obsoletos e desnecessários dos cursos
    - Excluídos arquivos de resumo e componentes não utilizados dos cursos de JavaScript, Next.js, Python, React e TypeScript.
    - Atualizações na estrutura do projeto para refletir as remoções e garantir a consistência do código.
    - Melhorias na organização dos arquivos para facilitar a manutenção futura.

--- public/courses/python-basics/index.ts
+++
@@ -1,9 +0,0 @@
-// coloca tipagem .json para poder importar
-import meta from "./summary.json"
-import cover from "./course-cover.png"
-
-export default {
-  ...meta,
-  slug: "python-basics", // ou meta.slug
-  cover,                 // string gerada pelo bundle
-}
